45bd3705886413cabf9d66a2c22b67ef0ae3cea8
Yves Isite <61538146+IsiteYves@users.noreply.github.com>
2021-06-10 22:04:41 +0200

    Update ProvideCode.php

--- application/controllers/ProvideCode.php
+++ application/controllers/ProvideCode.php
@@ -6,12 +6,32 @@ class ProvideCode extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('EventModel');
+		$this->load->model('User_model');
+		$this->load->library('form_validation');
 	}
 
 	function index()
 	{
-		$data['data'] = $this->EventModel->select();
-		$this->load->view('ResetPassword/ProvideCode', $data);
+		$this->load->view('ResetPassword/ProvideCode');
+	}
+
+	function validation()
+	{
+		$this->form_validation->set_rules('prov_code', 'Reset-Code', 'required|trim');
+		$reset_email = $this->input->post('email');
+		$v_code = $this->User_model->selectVCodeWhere($reset_email);
+		$prov_code = $this->input->post('prov_code');
+		$this->session->set_flashdata('email', $reset_email);
+		if ($this->form_validation->run()) {
+			if ($v_code !== $prov_code) {
+				$this->session->set_flashdata('vcode_error', "Incorrect code. Please try again.");
+				redirect('providecode');
+			} else {
+				$data['data'] = "$reset_email";
+				$this->load->view('ResetPassword/ResetPassword', $reset_email);
+			}
+		} else {
+			$this->index();
+		}
 	}
 }
