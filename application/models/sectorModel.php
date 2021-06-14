d7a3b3019d19a1b75bcf1d06871e892ff97cc6f8
Yves Isite <yvesisite@gmail.com>
2021-06-14 20:22:08 +0200

    Removed composer.lock from .gitignore

--- application/models/sectorModel.php
+++ application/models/sectorModel.php
@@ -9,6 +9,7 @@ class SectorModel extends CI_Model
 
 	function getSpecificSectors($districtId) {
 		$this->db->where('districtId', $districtId);
+		$this->db->order_by('sectorName');
 		$matching_sectors = $this->db->get('sectors')->result_array();
 		$whole_result = "";
 		foreach($matching_sectors as $sector) {
